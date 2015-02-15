6ac455c1e6bb020d61beeda5244f9eade96f4cd1
kylinfish <kylinfish@126.com>
2015-02-15 21:03:59 +0800

    Fixed: some text rename error fixed.

--- project_demo/project/urls.py
+++ project_demo/project/urls.py
@@ -7,7 +7,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
                        # Examples:
-                       # url(r'^$', 'testss.views.home', name='home'),
                        # url(r'^blog/', include('blog.urls')),
 
                        url(r'^admin/', include(admin.site.urls)),
--- project_demo/project/wsgi.py
+++ project_demo/project/wsgi.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-WSGI config for testss project.
+WSGI config for project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testss.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
